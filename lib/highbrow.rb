0e3d1bae64d2576f373235c980a166f573e1ecdf
Krzysztof Magosa <krzysztof@magosa.pl>
2015-09-26 18:47:06 +0200

    Add activation function base class

--- lib/highbrow.rb
+++ lib/highbrow.rb
@@ -3,3 +3,5 @@ require_relative 'highbrow/neural/error/mse'
 require_relative 'highbrow/neural/neuron'
 require_relative 'highbrow/neural/connection'
 require_relative 'highbrow/neural/layer'
+
+require_relative 'highbrow/neural/activation/base'
