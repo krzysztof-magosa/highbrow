63897a91a7ebbdb8567cb12ef2657fb01deac83f
Krzysztof Magosa <krzysztof@magosa.pl>
2015-09-26 18:52:59 +0200

    Add feed forward network class

--- lib/highbrow.rb
+++ lib/highbrow.rb
@@ -4,3 +4,5 @@ require_relative 'highbrow/neural/layer'
 
 require_relative 'highbrow/neural/activation/base'
 require_relative 'highbrow/neural/activation/sigmoid'
+
+require_relative 'highbrow/neural/network/feed_forward'
